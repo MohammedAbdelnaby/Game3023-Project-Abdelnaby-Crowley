b2b46025daf2ff5643455219e12d5485e204f621
Mohammed <mohammed.abdelnaby@georgebrown.ca>
2022-12-10 21:34:27 -0500

    added player movement

---
+++ Assets/[Scripts]/PlayerAnimationState.cs
@@ -0,0 +1,13 @@
+[System.Serializable]
+public enum PlayerAnimationState
+{
+    NONE,
+    WALK_NORTH,
+    WALK_EAST,
+    WALK_SOUTH,
+    WALK_WEST,
+    IDLE_NORTH,
+    IDLE_EAST,
+    IDLE_SOUTH,
+    IDLE_WEST,
+}
--- Assets/[Scripts]/PlayerMovement.cs
+++ Assets/[Scripts]/PlayerMovement.cs
@@ -7,12 +7,23 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float speed;
 
+    private Animator animator;
+
+    private PlayerAnimationState direction;
+
     public float Speed { get => speed; set => speed = value; }
+
+    private void Start()
+    {
+        direction = PlayerAnimationState.IDLE_SOUTH;
+        animator = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         Move();
+        UpdateAnimation();
     }
 
     private void Move()
@@ -21,5 +32,60 @@ public class PlayerMovement : MonoBehaviour
         float Y = Input.GetAxisRaw("Vertical");
 
         transform.position += new Vector3(X * Speed * Time.fixedDeltaTime, Y * Speed * Time.fixedDeltaTime, 0.0f);
+    }
+
+    private void ChangeAnimationDirectionState(PlayerAnimationState state)
+    {
+        direction = state;
+        animator.SetInteger("State", (int)direction);
+    }
+
+    private void UpdateAnimation()
+    {
+        float X = Input.GetAxisRaw("Horizontal");
+        float Y = Input.GetAxisRaw("Vertical");
+        Debug.Log(X + " , " + Y);
+        //Walking
+        //North
+        if (Y > 0.0f)
+        {
+            ChangeAnimationDirectionState(PlayerAnimationState.WALK_NORTH);
+        }
+        //East
+        else if (X > 0.0f)
+        {
+            ChangeAnimationDirectionState(PlayerAnimationState.WALK_EAST);
+        }
+        //South
+        else if(Y < 0.0f)
+        {
+            ChangeAnimationDirectionState(PlayerAnimationState.WALK_SOUTH);
+        }
+        //West
+        else if(X < 0.0f)
+        {
+            ChangeAnimationDirectionState(PlayerAnimationState.WALK_WEST);
+        }//IDLE
+        else if (X == 0.0f && Y == 0.0f)
+        {
+            switch (direction)
+            {
+                case PlayerAnimationState.WALK_NORTH:
+                    animator.SetInteger("State", (int)PlayerAnimationState.IDLE_NORTH);
+                    break;
+                case PlayerAnimationState.WALK_EAST:
+                    animator.SetInteger("State", (int)PlayerAnimationState.IDLE_EAST);
+                    break;
+                case PlayerAnimationState.WALK_SOUTH:
+                    animator.SetInteger("State", (int)PlayerAnimationState.IDLE_SOUTH);
+                    break;
+                case PlayerAnimationState.WALK_WEST:
+                    animator.SetInteger("State", (int)PlayerAnimationState.IDLE_WEST);
+                    break;
+                default:
+                    break;
+            }
+        }
+
     }
 }
